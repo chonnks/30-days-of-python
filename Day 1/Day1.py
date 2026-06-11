e98005a9a52d3b831337a1d93832b728fb5baeee
Chonnks <136377272+chonnks@users.noreply.github.com>
2026-06-11 19:40:30 -0400

    day one done learned about operators and data types

---
+++ Day 1/Day1.py
@@ -0,0 +1,25 @@
+#Introduction
+print("Hello World") # prints hello world
+
+print(2 + 3) # addition
+print(3 - 1) # subtraction
+print(2 * 3) # multiplication
+print(3 / 2) # division, returns float
+print(3 ** 2) # exponential
+print (3 % 2) # modulus
+print (3 // 2) # floor division
+
+#data types (type) gives data type
+print(type(10)) #Int
+print(type(3.14)) #float
+print(type(1+3j)) #complex
+print(type('Chonnks')) #String
+print(type([1,2,3])) #List
+print(type({'name': 'Chonks'})) # dictionary
+print(type({9.8, 3.14, 2.7})) #tuple or set
+
+#Notes
+#tuple and lists are the same thing, the only difference is that:
+#tuples are immutable and lists are mutable
+#If there is no order dependencies, default lists.
+# If there are order dependencies, use tuples.
